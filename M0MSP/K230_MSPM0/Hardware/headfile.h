8aad704c8a205690f8d0e3ef97ac73934d68b1bf
MADAO <3335075714@qq.com>
2025-07-20 17:49:38 +0800

    add K230 Uart test

---
+++ M0MSP/K230_MSPM0/Hardware/headfile.h
@@ -0,0 +1,17 @@
+#ifndef __HEADFILE_H__
+#define __HEADFILE_H__
+
+#include "encoder.h"
+#include "led.h"
+#include "stdio.h"
+#include "string.h"
+#include "ti_msp_dl_config.h"
+#include "key.h"
+#include "motor.h"
+#include "encoder.h"
+#include "stdlib.h"
+#include "math.h"
+
+
+
+#endif
